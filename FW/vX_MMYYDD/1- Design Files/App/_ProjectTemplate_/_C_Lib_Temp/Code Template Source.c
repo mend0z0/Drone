610db349c26072b0e0d7a58183d0f22fe7b80bb7
Mend0z0 <40163097+mend0z0@users.noreply.github.com>
2023-09-26 15:15:04 -0400

    Initial commit

---
+++ FW/vX_MMYYDD/1- Design Files/App/_ProjectTemplate_/_C_Lib_Temp/Code Template Source.c
@@ -0,0 +1,132 @@
+/****************************************************************************************************
+*					©2022 - 2032 Siavash Taher Parvar All Rights Reserved.
+*
+*				Project Name:
+*		  @Brief Description:
+*				 File Status:	DRAFT
+*						(DRAFT , PRILIMINARY, CHECKED, RELEASED)
+*	File Name:	xxxx.c
+*	Version:	xx
+*	Revision:	xx
+*	Date:		mm/dd/yyyy
+*   Company:    Mend0z0
+*	Client:     ----------
+*	License:	Private License (Contact for more info.)
+*	******************************** Project Description *******************************************
+*	@Detailed Description:
+*
+*
+*
+*	******************************** File Description *******************************************
+*	@Detailed Description:
+*
+*
+*
+*	*********************************  Processor Info **********************************************
+*	Processor:					x-Bits
+*	Clock Frequency:	00.000	xHz
+*	RAM/SRAM Size:		00.00	MBytes
+*	Flash Size:			00.00	MBytes
+*	Cache Size:			00.00	MBytes
+*	***********************************  Case Style  ***********************************************
+*	Local Variables = camelCase			Global Variables = g_camelCase
+*	Headers = SCREAMIN_SNAKE_CASE		Const Variables = SCREAMING_SNAKE_CASE		Macros = SCREAMING_SNAKE_CASE
+*	Functions = PascalCase				Initialize Functions = _lower_case
+*	Struct = lower_case					Struct.members = camelCase
+*	Enum = lower_case					Enum Members = SNAKE_CASE
+*	Pointer Variables = p_camelCase
+*	*********************************** Contact Info ***********************************************
+*	Author:	Siavash Taher Parvar
+*	Email:  s.taherparvar@gmail.com
+*	Github: github.com/mend0z0
+*						CONFIDENTIAL DOCUMENT, DO NOT DISTRIBUTE
+****************************************************************************************************/
+
+/****************************************************************************************************
+****************************       HEADERS DECLARATION       ****************************************
+*****************************************************************************************************/
+
+/****************************************************************************************************
+****************************   GLOB. VARIABLES DECLARATION    ***************************************
+*****************************************************************************************************/
+
+/****************************************************************************************************
+**************************     STATIC FUNCTIONS DECLARATION      ************************************
+*****************************************************************************************************/
+
+/****************************************************************************************************
+****************************         GLOBAL FUNTIONS         ****************************************
+*****************************************************************************************************/
+
+/****************************************************************************************************
+*				©2022 - 2032 Siavash Taher Parvar All Rights Reserved.
+*
+*		 @Brief Description:
+*			Function Status: 	DRAFT
+*					(DRAFT , PRILIMINARY, CHECKED, RELEASED)
+*	************************************************************************************************
+*	Author:		Siavash Taher Parvar					Checked By:
+*	Date:		mm/dd/yyyy
+*	Version:	xx
+*	Revision:	xx
+*	************************************************************************************************
+*	Function Name:
+*	Function Scope:			Global
+*	Function Parameters:
+*	Function Return Type:
+*	************************************************************************************************
+*	@Detailed Description: (Do numbering and tag the number to each part of code)
+*	(1)
+*	(2)
+*	(3)
+*	.
+*	.
+*	.
+*	************************************************************************************************
+*	Revision History (Description, author, date: mm/dd/yyyy)
+*
+*	************************************************************************************************
+*	License: Private License (Contact for more info.)
+*	Email: s.taherparvar@gmail.com
+****************************************************************************************************/
+
+
+/****************************************************************************************************
+****************************         STATIC FUNTIONS         ****************************************
+*****************************************************************************************************/
+
+/****************************************************************************************************
+*				©2022 - 2032 Siavash Taher Parvar All Rights Reserved.
+*
+*		 @Brief Description:
+*			Function Status: 	DRAFT
+*					(DRAFT , PRILIMINARY, CHECKED, RELEASED)
+*	************************************************************************************************
+*	Author:		Siavash Taher Parvar					Checked By:
+*	Date:		mm/dd/yyyy
+*	Version:	xx
+*	Revision:	xx
+*	************************************************************************************************
+*	Function Name:
+*	Function Scope:			Local(static)
+*	Function Parameters:
+*	Function Return Type:
+*	************************************************************************************************
+*	@Detailed Description: (Do numbering and tag the number to each part of code)
+*	(1)
+*	(2)
+*	(3)
+*	.
+*	.
+*	.
+*	************************************************************************************************
+*	Revision History (Description, author, date: mm/dd/yyyy)
+*
+*	************************************************************************************************
+*	License: Private License (Contact for more info.)
+*	Email: s.taherparvar@gmail.com
+****************************************************************************************************/
+
+
+/**************************   (C)SIAVASH TAHER PARVAR ALL RIGHTS RESERVED   ************************/
+/************************************     END OF THE FILE      *************************************/
---
+++ README.md
@@ -0,0 +1,2 @@
+# QuadCopter
+ QuadCopter Project
