d8034aa64d122129345ed8ebf2f423cad401695b
Mend0z0 <40163097+mend0z0@users.noreply.github.com>
2024-01-13 21:55:03 -0500

    Flight controller firmware template has been added

--- Firmware/_Sub_FW_FlightController/v1_20230113/Design Folder/_C_Lib_Temp/Code Template Header.h
+++ Firmware/_Sub_FW_FlightController/v1_20230113/Design Folder/_C_Lib_Temp/Code Template Header.h
@@ -1,32 +1,33 @@
 /****************************************************************************************************
 *
-*   Project Name:
-*   @Brief Description:
+*   Project Name:       Quadcopter.Flight Controller
+*   @Brief Description: The flight controller firmware part of Quadcopter project
 *   File Status:	    DRAFT   (DRAFT , PRELIMINARY, CHECKED, RELEASED)
 *
-*	File Name:	xxxx.c
-*	Version:	xx
-*	Revision:	xx
-*	Date:		yyyy/mm/dd
+*	File Name:	xxxx.h
+*	Version:	01
+*	Revision:	01
+*	Date:		2023/01/dd
 *	License:	Open-source
 *	******************************** Project Description *******************************************
-*	@Detailed Description:
-*
-*
+*	@Detailed Description: Flight controller project is part of a bigger project that is called Quadcopter.
+*   In this project, we try to control the motors by a PID control loop and the set-point value that will
+*   be received from Radio controller/LoRaWAN Controller (Other projects). In this project, the x-y-z
+*   coordinate points will be read from IMU sensor and the motors will be controlled by PWM signals.
 *
 *	******************************** File Description *******************************************
 *	@Detailed Description:
 *
 *
+*
 *	*********************************  Hardware Info  **********************************************
-*   Name:
-*   Version:
+*   Name:       _HW_FlightController
+*   Version:    v1.0
 *	*********************************  Processor Info **********************************************
-*	Processor:          x-Bits
-*	Clock Frequency:    00.000	xHz
-*	RAM/SRAM Size:      00.00	MBytes
-*	Flash Size:         00.00	MBytes
-*	Cache Size:         00.00	MBytes
+*	Processor:          STM32U545 (32-Bits)
+*	Clock Frequency:    160.000	MHz
+*	RAM/SRAM Size:      2.50	KBytes
+*	Flash Size:         512.00	KBytes
 *	***********************************  Case Style  ***********************************************
 *	Local Variables = camelCase			Global Variables = g_camelCase
 *	Headers = SCREAMIN_SNAKE_CASE
@@ -37,82 +38,43 @@
 *	Enum = lower_case					Enum Members = SNAKE_CASE
 *	Pointer Variables = p_camelCase
 *	*********************************** Contact Info ***********************************************
-*	Author:	Siavash Taher Parvar
-*	GitHub: github.com/Mend0z0
+*	Author: Siavash Taher Parvar
+*	GitHub: www.github.com/Mend0z0
 ****************************************************************************************************/
-
+#ifndef CODETEMPLATEHEADER_H_
+#define CODETEMPLATEHEADER_H_
 /****************************************************************************************************
 ****************************       HEADERS DECLARATION       ****************************************
 *****************************************************************************************************/
 
 /****************************************************************************************************
-****************************   GLOB. VARIABLES DECLARATION    ***************************************
+****************************        MACRO DECLARATION         ***************************************
 *****************************************************************************************************/
 
 /****************************************************************************************************
-***********************     STATIC/LOCAL FUNCTIONS DECLARATION      *********************************
+****************************   CONST VARIABLES DECLARATION    ***************************************
 *****************************************************************************************************/
 
 /****************************************************************************************************
-****************************         GLOBAL FUNTIONS         ****************************************
+**************************     GLOBAL FUNCTIONS DECLARATION      ************************************
 *****************************************************************************************************/
 
-/****************************************************************************************************
-****************************         STATIC FUNTIONS         ****************************************
-*****************************************************************************************************/
 
-/***************************************************************************************************/
-/**********************************                             ************************************/
-/********************                     END OF THE CODE                         ******************/
-/**********************************                             ************************************/
-/***************************************************************************************************/
-
-/*                                   GLOBAL FUNCTION TEMPLATE                                      */
+#endif /* CODETEMPLATEHEADER_H_  */
 
 /****************************************************************************************************
-*   @Brief Description:
-*   Function Status: 	DRAFT   (DRAFT , PRILIMINARY, CHECKED, RELEASED)
-*
-*	************************************************************************************************
-*	Function Name:
-*	Function Scope:			Global
-*	Function Parameters:
-*	Function Return Type:
-*	************************************************************************************************
-*	@Detailed Description: (Do numbering and tag the number to each part of code)
-*	(1)
-*	(2)
-*	(3)
-*	.
-*	.
-*	.
-*	************************************************************************************************
-*	Revision History (Description, author, date: yyyy/mm/dd)
+*	Revision History (Description (author, date: yyyy/mm/dd))
 *
 ****************************************************************************************************/
 
-/*                               STATIC/LOCAL FUNCTION TEMPLATE                                    */
-
+/*                                 FUNCTION DECLARATION TEMPLATE                                   */
 /****************************************************************************************************
 *   @Brief Description:
-*   Function Status: 	DRAFT   (DRAFT , PRELIMINARY, CHECKED, RELEASED)
 *
 *	************************************************************************************************
 *	Function Name:
-*	Function Scope:         Local(static)
 *	Function Parameters:
 *	Function Return Type:
-*	************************************************************************************************
-*	@Detailed Description: (Do numbering and tag the number to each part of code)
-*	(1)
-*	(2)
-*	(3)
-*	.
-*	.
-*	.
-*	************************************************************************************************
-*	Revision History (Description (author, date: yyyy/mm/dd))
-*
 ****************************************************************************************************/
 
 /************************************     END OF THE FILE      *************************************/
---
+++ Firmware/_Sub_FW_FlightController/v1_20230113/Design Folder/_C_Lib_Temp/Code Template Source.c
@@ -0,0 +1,118 @@
+/****************************************************************************************************
+*
+*   Project Name:       Quadcopter.Flight Controller
+*   @Brief Description: The flight controller firmware part of Quadcopter project
+*   File Status:	    DRAFT   (DRAFT , PRELIMINARY, CHECKED, RELEASED)
+*
+*	File Name:	xxxx.c
+*	Version:	01
+*	Revision:	01
+*	Date:		2023/01/dd
+*	License:	Open-source
+*	******************************** Project Description *******************************************
+*	@Detailed Description: Flight controller project is part of a bigger project that is called Quadcopter.
+*   In this project, we try to control the motors by a PID control loop and the set-point value that will
+*   be received from Radio controller/LoRaWAN Controller (Other projects). In this project, the x-y-z
+*   coordinate points will be read from IMU sensor and the motors will be controlled by PWM signals.
+*
+*	******************************** File Description *******************************************
+*	@Detailed Description:
+*
+*
+*	*********************************  Hardware Info  **********************************************
+*   Name:       _HW_FlightController
+*   Version:    v1.0
+*	*********************************  Processor Info **********************************************
+*	Processor:          STM32U545 (32-Bits)
+*	Clock Frequency:    160.000	MHz
+*	RAM/SRAM Size:      2.50	KBytes
+*	Flash Size:         512.00	KBytes
+*	***********************************  Case Style  ***********************************************
+*	Local Variables = camelCase			Global Variables = g_camelCase
+*	Headers = SCREAMIN_SNAKE_CASE
+*   Macros = SCREAMING_SNAKE_CASE
+*   Const Variables = SCREAMING_SNAKE_CASE
+*	Functions = PascalCase				Initialize Functions = _lower_case
+*	Struct = lower_case					Struct.members = camelCase
+*	Enum = lower_case					Enum Members = SNAKE_CASE
+*	Pointer Variables = p_camelCase
+*	*********************************** Contact Info ***********************************************
+*	Author:	Siavash Taher Parvar
+*	GitHub: github.com/Mend0z0
+****************************************************************************************************/
+
+/****************************************************************************************************
+****************************       HEADERS DECLARATION       ****************************************
+*****************************************************************************************************/
+
+/****************************************************************************************************
+****************************   GLOB. VARIABLES DECLARATION    ***************************************
+*****************************************************************************************************/
+
+/****************************************************************************************************
+***********************     STATIC/LOCAL FUNCTIONS DECLARATION      *********************************
+*****************************************************************************************************/
+
+/****************************************************************************************************
+****************************         GLOBAL FUNTIONS         ****************************************
+*****************************************************************************************************/
+
+/****************************************************************************************************
+****************************         STATIC FUNTIONS         ****************************************
+*****************************************************************************************************/
+
+/***************************************************************************************************/
+/**********************************                             ************************************/
+/********************                     END OF THE CODE                         ******************/
+/**********************************                             ************************************/
+/***************************************************************************************************/
+
+/*                                   GLOBAL FUNCTION TEMPLATE                                      */
+
+/****************************************************************************************************
+*   @Brief Description:
+*   Function Status: 	DRAFT   (DRAFT , PRILIMINARY, CHECKED, RELEASED)
+*
+*	************************************************************************************************
+*	Function Name:
+*	Function Scope:			Global
+*	Function Parameters:
+*	Function Return Type:
+*	************************************************************************************************
+*	@Detailed Description: (Do numbering and tag the number to each part of code)
+*	(1)
+*	(2)
+*	(3)
+*	.
+*	.
+*	.
+*	************************************************************************************************
+*	Revision History (Description, author, date: yyyy/mm/dd)
+*
+****************************************************************************************************/
+
+/*                               STATIC/LOCAL FUNCTION TEMPLATE                                    */
+
+/****************************************************************************************************
+*   @Brief Description:
+*   Function Status: 	DRAFT   (DRAFT , PRELIMINARY, CHECKED, RELEASED)
+*
+*	************************************************************************************************
+*	Function Name:
+*	Function Scope:         Local(static)
+*	Function Parameters:
+*	Function Return Type:
+*	************************************************************************************************
+*	@Detailed Description: (Do numbering and tag the number to each part of code)
+*	(1)
+*	(2)
+*	(3)
+*	.
+*	.
+*	.
+*	************************************************************************************************
+*	Revision History (Description (author, date: yyyy/mm/dd))
+*
+****************************************************************************************************/
+
+/************************************     END OF THE FILE      *************************************/
