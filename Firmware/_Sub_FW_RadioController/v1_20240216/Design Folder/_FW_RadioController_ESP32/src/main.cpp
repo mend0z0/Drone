9d40112ffe2966e0fd315813788a2622f940c8f1
Mend0z0 <40163097+mend0z0@users.noreply.github.com>
2024-02-18 22:59:31 -0500

    ESP32 Firmware is successfully pub&sub on MQTT, Applicaiton also tested to work properly.

--- Firmware/_Sub_FW_RadioController/v1_20240216/Design Folder/_FW_RadioController_ESP32/src/main.cpp
+++ Firmware/_Sub_FW_RadioController/v1_20240216/Design Folder/_FW_RadioController_ESP32/src/main.cpp
@@ -1,10 +1,44 @@
 #include <Arduino.h>
+#include <WiFi.h>
 #include <EspMQTTClient.h>
 
+
+EspMQTTClient radioController(
+    "55Sundance",               // Wifi SSID
+    "4168309434",               // Wifi password
+    "192.168.0.148",            // MQTT Broker IP
+    "",                         // MQTT Username
+    "",                         // MQTT Password
+    "Quadcopter",               // MQTT Client name
+    3667                        // MQTT port number
+);
+
+String returnData;
+
 void setup() {
-  
+    Serial.begin(9600);
+
+    radioController.enableDebuggingMessages();   // Enable debugging messages sent to serial output
+    radioController.enableHTTPWebUpdater();      // Enable the web updater. User and password default to values of MQTTUsername and MQTTPassword. These can be overridded with enableHTTPWebUpdater("user", "password").
+}
+
+void onConnectionEstablished()
+{
+  // Subscribe to "QCOPTER0" and display received message to Serial
+  radioController.subscribe("QCOPTER0/#", [](const String & payload) {
+    returnData  = payload;
+    Serial.println(payload);
+  });
+
+  // Publish a message to "QCOPTER0"
+  if(returnData){
+    radioController.publish("QCOPTER0", returnData); // You can activate the retain flag by setting the third parameter to true
+    returnData.clear(); 
+  }
+
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  radioController.loop();
 }
