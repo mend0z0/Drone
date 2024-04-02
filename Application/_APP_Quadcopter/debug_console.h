75b61526b26a0541e87963fbb556476869f82006
Mend0z0 <40163097+mend0z0@users.noreply.github.com>
2024-04-02 13:37:47 -0400

    Update debug_console.h

--- Application/_APP_Quadcopter/debug_console.h
+++ Application/_APP_Quadcopter/debug_console.h
@@ -2,6 +2,15 @@
 #define DEBUG_CONSOLE_H
 
 #include <QDialog>
+#include <QDebug>
+#include <QByteArray>
+#include <QString>
+#include <QIODevice>
+#include <QSerialPort>
+#include <QSerialPortInfo>
+#include <QDateTime>
+#include <QFileDialog>
+#include <QMessageBox>
 
 namespace Ui {
 class debug_console;
@@ -15,8 +24,39 @@ public:
     explicit debug_console(QWidget *parent = nullptr);
     ~debug_console();
 
+signals:
+
+    void DebugPortConnectStatus( bool );
+
+public slots:
+
+private slots:
+
+    void SerialPortDataRead( void );
+
+    void SetSerialPort( int index );
+    void SetBaudRate( int index );
+    void SetNBits( int index );
+    void SetParity( int index );
+    void SetStopBits( int index );
+
+    void SerialPortConnection( void );
+
+    void SerialRxClear( void );
+    void SerialRxPause( void );
+    void SerialRxSave( void );
+
+    void SerialTxCMDSend( void );
+    void SerialTxCMDClear( void );
+
 private:
     Ui::debug_console *ui;
+
+    QSerialPort *debugSerialPort = new QSerialPort(this);
+
+    void DebugModeParamInit( void );
+    void DebugModeConnectingFunctions( void );
+
 };
 
 #endif // DEBUG_CONSOLE_H
