5421da03163d1c693a5ca55bd8a388b60eb647d5
Mend0z0 <40163097+mend0z0@users.noreply.github.com>
2024-05-25 15:28:50 -0400

    Update _drv_lpuart1.c

--- Firmware/_SUB_FW_EquilibriumController/v1_20240506/Design Folder/_FW_EquilibriumController/Drivers/_drv_LPUART1/_drv_lpuart1.c
+++ Firmware/_SUB_FW_EquilibriumController/v1_20240506/Design Folder/_FW_EquilibriumController/Drivers/_drv_LPUART1/_drv_lpuart1.c
@@ -50,7 +50,7 @@
 ****************************   CONST VARIABLES DECLARATION    ***************************************
 *****************************************************************************************************/
 
-const uint16_t LPUART_BUFF_SIZE_RX = 254;		//it's 256 bytes
+const uint16_t LPUART_BUFF_SIZE_RX = 128;		//it's 128 bytes
 const uint16_t LPUART_BUFF_SIZE_TX = 1024;		//it's 1024 bytes
 
 /****************************************************************************************************
@@ -166,8 +166,7 @@ void LPDMA1_Channel1_IRQHandler( void )
 ***********************     STATIC/LOCAL FUNCTIONS DECLARATION      *********************************
 *****************************************************************************************************/
 
-void _init_Clock( void );
-void _init_LPGPIO( void );
+void _init_GPIO( void );
 void _init_LPTIM1( void );
 void _init_LPDMA1( void );
 
@@ -194,12 +193,27 @@ void _init_LPDMA1( void );
 ****************************************************************************************************/
 void _init_lpuart1( void )
 {
-	_init_Clock();
 	_init_LPGPIO();
 	_init_LPTIM1();
 	_init_LPDMA1();
 
 	//init the LPUART1
+	RCC->CCIPR3 |= RCC_CCIPR3_LPUART1SEL_1;		// HSI16 selected
+
+	//Set the baud rate
+	LPUART1->PRESC = USART_PRESC_PRESCALER_3;	// 16 MHz / 32
+	LPUART1->BRR = 13333;						// It'll make the baud-rate to be ((16 MHz * 256) / 32) / 13333 ~= 9600
+
+	LPUART1->CR2 |= USART_CR2_STOP_1;			// 2 stop bits
+
+	LPUART1->CR1 |= USART_CR1_RXFFIE;			// An LPUART interrupt is generated when RXFF=1 in the LPUART_ISR register
+	LPUART1->CR1 |= USART_CR1_RE;				// Receiver is enabled and begins searching for a start bit
+	LPUART1->CR3 |= USART_CR3_DMAR;				// DMA mode is enabled for reception
+
+	LPUART1->CR1 |= USART_CR1_TE;				// Transmitter is enabled
+	LPUART1->CR3 |= USART_CR3_DMAT;				// DMA mode is enabled for transmission
+
+	LPUART1->CR1 |= USART_CR1_UE;				// LPUART enabled
 }
 
 /****************************************************************************************************
@@ -334,44 +348,23 @@ void LPUARTFlushTx( void )
 *	Revision History (Description (author, date: yyyy/mm/dd))
 *
 ****************************************************************************************************/
-void _init_Clock( void )
-{
-
-}
-
-/****************************************************************************************************
-*   @Brief Description:
-*   Function Status: 	DRAFT   (DRAFT , PRELIMINARY, CHECKED, RELEASED)
-*
-*	************************************************************************************************
-*	Function Name:
-*	Function Scope:         Local(static)
-*	Function Parameters:
-*	Function Return Type:
-*	************************************************************************************************
-*	@Detailed Description: (Do numbering and tag the number to each part of code)
-*   Function Parameters Description:
-*   Function Return Type Description:
-*   Comments should be added in front of each line. (This line should be deleted)
-*	************************************************************************************************
-*	Revision History (Description (author, date: yyyy/mm/dd))
-*
-****************************************************************************************************/
 void _init_GPIO( void )
 {
+	RCC->AHB2ENR1 |= RCC_AHB2ENR1_GPIOAEN;								// IO port A clock enabled
+
 	//PA2 = LPUART1_TX & IT'S AF8
 	//PA3 = LPUART1_RX & IT'S AF8
-	GPIOA->MODER |= GPIO_MODER_MODE2_1 | GPIO_MODER_MODE3_1;			//Set the GPIO mode as Alternate function
-	GPIOA->OTYPER &= ~(GPIO_OTYPER_OT2 | GPIO_OTYPER_OT3);				//Set the output push-pull
-	GPIOA->OSPEEDR &= ~(GPIO_OSPEEDR_OSPEED2 | GPIO_OSPEEDR_OSPEED3);	//Set the speed as low speed
-	GPIOA->PUPDR &= ~(GPIO_PUPDR_PUPD2 | GPIO_PUPDR_PUPD3);				//No pull-up, pull-down
+	GPIOA->MODER |= GPIO_MODER_MODE2_1 | GPIO_MODER_MODE3_1;			// Set the GPIO mode as Alternate function
+	GPIOA->OTYPER &= ~(GPIO_OTYPER_OT2 | GPIO_OTYPER_OT3);				// Set the output push-pull
+	GPIOA->OSPEEDR &= ~(GPIO_OSPEEDR_OSPEED2 | GPIO_OSPEEDR_OSPEED3);	// Set the speed as low speed
+	GPIOA->PUPDR &= ~(GPIO_PUPDR_PUPD2 | GPIO_PUPDR_PUPD3);				// No pull-up, pull-down
 
-	GPIOA->AFR[0] |= GPIO_AFRL_AFSEL2_3 | GPIO_AFRL_AFSEL3_3;			//Set the Alternate function to AF8.
+	GPIOA->AFR[0] |= GPIO_AFRL_AFSEL2_3 | GPIO_AFRL_AFSEL3_3;			// Set the Alternate function to AF8.
 
 	//Locking the port bits
-	GPIOA->LCKR |= GPIO_LCKR_LCK2 | GPIO_LCKR_LCK3 | GPIO_LCKR_LCKK;	//1
-	GPIOA->LCKR |= GPIO_LCKR_LCK2 | GPIO_LCKR_LCK3;						//2
-	GPIOA->LCKR |= GPIO_LCKR_LCK2 | GPIO_LCKR_LCK3 | GPIO_LCKR_LCKK;	//3
+	GPIOA->LCKR |= GPIO_LCKR_LCK2 | GPIO_LCKR_LCK3 | GPIO_LCKR_LCKK;	// 1
+	GPIOA->LCKR |= GPIO_LCKR_LCK2 | GPIO_LCKR_LCK3;						// 2
+	GPIOA->LCKR |= GPIO_LCKR_LCK2 | GPIO_LCKR_LCK3 | GPIO_LCKR_LCKK;	// 3
 }
 
 /****************************************************************************************************
@@ -394,15 +387,25 @@ void _init_GPIO( void )
 ****************************************************************************************************/
 void _init_LPTIM1( void )
 {
-	LPTIM1->CFGR |= LPTIM_CFGR_PRESC;	// Config the prescaler...
+	RCC->CCIPR3 |= RCC_CCIPR3_LPTIM1SEL_1;		// HSI16 selected
 
-	LPTIM1->CFGR |= LPTIM_CFGR_PRELOAD;	// Registers are updated at the end of the current LPTIM period
+	LPTIM1->CFGR |= LPTIM_CFGR_PRESC;			// Input clock is 32MHz so PRESC = 32MHz/128 = 250 kHz
 
-	LPTIM1->ARR = 0XFFFF;				// Value for this bit should be set later...
+	LPTIM1->CFGR |= LPTIM_CFGR_PRELOAD;			// Registers are updated at the end of the current LPTIM period
 
-	LPTIM1->CR |= LPTIM_CR_ENABLE | LPTIM_CR_SNGSTRT;		// LPTIM is enabled + LPTIM start in Single mode
+	LPTIM1->ARR = 40000;						// (1.25 ms(LPUART Baud rate) * 128(BuffSize)) / (1 / (32 MHz (in_clk) / 128 (pre-scaler))) = 40000 :)
 
-	LPTIM1->DIER |= LPTIM_DIER_UEIE;	//Enable Update Event Interrupt. This line should be placed after the LPTIM is enabled...
+	LPTIM1->CR |= LPTIM_CR_ENABLE;				// LPTIM is enabled
+
+	LPTIM1->DIER |= LPTIM_DIER_UEIE;			// Enable Update Event Interrupt. This line should be placed after the LPTIM is enabled.
+
+	while(!(LPTIM1->ISR & LPTIM_ISR_DIEROK))	// Waiting until the DIEROK sets so we can make sure the interrupt is enabled.
+	{
+		//maybe adding a timeout...
+	}
+	LPTIM1->ICR |= LPTIM_ICR_DIEROKCF;			// Writing 1 to this bit clears the DIEROK flag in the LPTIM_ISR register.
+
+	LPTIM1->CR &= ~LPTIM_CR_ENABLE;				// LPTIM is disabled
 }
 
 /****************************************************************************************************
